3818781a6a8e7ab74f55aaee85fa7a6320a62815
SayedMajid <sydmajid04@gmail.com>
2022-07-20 21:36:07 +0530

    Login functionality

---
+++ src/Context/Appcontext.jsx
@@ -0,0 +1,62 @@
+import { createContext, useState } from "react";
+
+export const Appcontext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const [loginStatus, setLoginStatus] = useState({
+    loggedIn: false,
+    token: null,
+  });
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLoginChange = (e) => {
+    let { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch("https://reqres.in/api/login", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(loginData),
+    })
+      .then((res) => res.json())
+      .then((res) =>
+        setLoginStatus({
+          loggedIn: true,
+          token: res.token,
+        })
+      );
+
+    console.log(loginStatus);
+  };
+
+  const handleLogout = () => {
+    setLoginStatus({
+      loggedIn: false,
+      token: null,
+    });
+  };
+
+  return (
+    <Appcontext.Provider
+      value={{
+        loginData,
+        handleLoginChange,
+        handleLogin,
+        loginStatus,
+        handleLogout,
+      }}
+    >
+      {children}
+    </Appcontext.Provider>
+  );
+};
